7b112fd1eae42063d7bf6ee1e21a391fe32730b6
Wojciech Paupa <wp431488@students.mimuw.edu.pl>
2024-10-11 16:21:23 +0200

    Poprawione JVM

--- JVMGen.hs
+++ JVMGen.hs
@@ -5,7 +5,7 @@ import Data.Map
 
 import AbsInstant
 
-data TCM a = State (Map Ident Integer, Integer) a 
+type TCM a = State (Map Ident Integer, Integer) a 
 
 runTCM :: TCM a -> (a, Integer)
 runTCM m = let (res, (_, d)) = runState m (empty, 0) in (res, d)
@@ -19,7 +19,7 @@ associate x = do
 getAssoc :: Ident -> TCM Integer
 getAssoc x = do
     (assocs, _) <- get
-    case lookup x assocs of
+    case Data.Map.lookup x assocs of
         Just assoc -> return assoc
         Nothing -> associate x
 
@@ -28,16 +28,17 @@ commute "idiv" = "swap\n\t"
 commute "isub" = "swap\n\t"
 commute _ = ""
 
+emitExp :: Exp -> TCM (Integer, String)
 emitExpGeneral :: String -> Exp -> Exp -> TCM (Integer, String)
+
 emitExpGeneral name e1 e2 = do
-    (d1, code1) <- emitExpGeneral e1
-    (d2, code2) <- emitExpGeneral e2
-    if d1 > d2 then 
+    (d1, code1) <- emitExp e1
+    (d2, code2) <- emitExp e2
+    if d1 >= d2 then 
         return (max d1 (1 + d2), code1 ++ code2 ++ name ++ "\n\t")
     else
         return (max d2 (1 + d1), code2 ++ code1 ++ commute name ++ name ++ "\n\t")
 
-emitExp :: Exp -> TCM (Integer, String)
 emitExp (ExpAdd e1 e2) = emitExpGeneral "iadd" e1 e2
 emitExp (ExpSub e1 e2) = emitExpGeneral "isub" e1 e2
 emitExp (ExpMul e1 e2) = emitExpGeneral "imul" e1 e2
@@ -55,9 +56,9 @@ emitExp (ExpVar v) = do
     assoc <- getAssoc v
     return (1,
             if 0 <= assoc && assoc <= 3 then
-                "iload_" ++ assoc ++ "\n\t"
+                "iload_" ++ show assoc ++ "\n\t"
             else
-                "iload " ++ assoc ++ "\n\t"
+                "iload " ++ show assoc ++ "\n\t"
         )
 
 emitStmt :: Stmt -> TCM (Integer, String)
@@ -65,7 +66,7 @@ emitStmt (SAss var expr) = do
     (d, code) <- emitExp expr
     assoc <- getAssoc var
     let space = if 0 <= assoc && assoc <= 3 then "_" else " "
-    return (d, code ++ "istore" ++ space ++ assoc ++ "\n\t")
+    return (d, code ++ "istore" ++ space ++ show assoc ++ "\n\t")
 emitStmt (SExp expr) = do
     (d, code) <- emitExp expr
     return (d + 1, "getstatic java/lang/System/out Ljava/io/PrintStream;\n\t" ++ code ++ "invokevirtual java/io/PrintStream/println(I)V\n\t")
@@ -75,13 +76,13 @@ genJVM :: [Stmt] -> String
 genJVM ss = 
     let codes = mapM emitStmt ss
         (cs, locals) = runTCM codes
-        compare (d1, _) (d2, _) = d1 >= d2
-        maxd = foldr (\x y -> if compare x y then x else y) cs
-        code = concat (map snd cs) in
+        maxd = Prelude.foldr (\x y -> if fst x >= y then fst x else y) 0 cs
+        code = concat (Prelude.map snd cs) in
     ".class public Main\n" ++
     ".super java/lang/Object\n\n" ++
     ".method public <init>()V\n\taload_0\n\tinvokespecial java/lang/Object/<init>()V\n\treturn\n.end method\n\n" ++
     ".method public static main([Ljava/lang/String;)V\n" ++ 
-    ".limit stack " ++ maxd ++ "\n\t" ++
+    ".limit stack " ++ show maxd ++ "\n" ++
+    ".limit locals " ++ show locals ++ "\n\t" ++
     code ++
     "return\n.end method"
